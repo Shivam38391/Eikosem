276cbd3ffb5b5fc0edd66e94e60a343a931b8694
shivam sharma <shivamsharma38391@gmail.com>
2023-04-26 00:11:07 +0530

    requirements and deployment settings

--- eiko/settings.py
+++ eiko/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,7 +44,6 @@ INSTALLED_APPS = [
     
         'rest_framework',
         'mytodoapp.apps.MytodoappConfig',
-        
         "django_filters"
 ]
 
@@ -80,11 +81,15 @@ WSGI_APPLICATION = "eiko.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
